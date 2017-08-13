ae3478de72ee439c5d90ee9a92799503ba83c1ae
Valentin Vago <zeropaper@irata.ch>
2017-08-13 07:33:15 +0200

    fix mongoose connection

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ module.exports = function rutServer(options, initFinished) {
 
   var server = options.server = http.createServer(app);
 
-  var db = mongoose.createConnection();
+  var db;
 
 
 
@@ -125,13 +125,8 @@ module.exports = function rutServer(options, initFinished) {
   //
   //
   setupOperations.db = function (setup, done) {
-    setup.db.open(options.mongodbURL, function (err) {
-      if (err) {
-        console.warn('mongodb connection "%s" could not be open\n %s', options.mongodbURL, err.stack);
-        return done(err);
-      }
-      done(null, setup);
-    });
+    db = setup.db = mongoose.createConnection(options.mongodbURL);
+    done(null, setup);
   };
 
 
